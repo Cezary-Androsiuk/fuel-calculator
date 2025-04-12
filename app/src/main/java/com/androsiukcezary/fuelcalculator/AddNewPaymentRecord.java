754d3bc56df78c0b5f1a9ef9baced9af335aab3b
Cezary Androsiuk PC <czandros@wp.pl>
2025-04-12 23:45:31 +0200

    implemented Refueling and Payment Records - fuck that member attribute prefixes...

--- app/src/main/java/com/androsiukcezary/fuelcalculator/AddNewPaymentRecord.java
+++ app/src/main/java/com/androsiukcezary/fuelcalculator/AddNewPaymentRecord.java
@@ -1,6 +1,8 @@
 package com.androsiukcezary.fuelcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +17,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddNewPaymentRecord extends AppCompatActivity {
 
-    EditText m_currentFuelEditText;
-    Button m_saveNewPaymentButton;
-    DateTimeStruct m_dateTime;
+    EditText moneyPaidEditText;
+    Button saveNewPaymentButton;
+    DateTimeStruct dateTime;
+
+    double moneyPaid;
+    TimeDateDataSet timeDateDataSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("ADD_NEW_PAYMENT_RECORD_ACTIVITY_LOGS", "onCreate");
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_new_payment_record);
@@ -33,11 +39,13 @@ public class AddNewPaymentRecord extends AppCompatActivity {
         ImageButton closeAddingPaymentButton = (ImageButton) findViewById(R.id.closeAddingPaymentButton);
         closeAddingPaymentButton.setOnClickListener(v -> this.closeActivity());
 
-        m_currentFuelEditText = (EditText) findViewById(R.id.currentFuelEditText);
-        m_saveNewPaymentButton = (Button) findViewById(R.id.saveNewPaymentButton);
+        timeDateDataSet = new TimeDateDataSet();
+
+        moneyPaidEditText = (EditText) findViewById(R.id.moneyPaidEditText);
+        saveNewPaymentButton = (Button) findViewById(R.id.saveNewPaymentButton);
 
         ///  create date time structure and assign views to them
-        m_dateTime = new DateTimeStruct(
+        dateTime = new DateTimeStruct(
                 this,
                 findViewById(R.id.timeEditText),
                 findViewById(R.id.timeSelectButton),
@@ -45,6 +53,58 @@ public class AddNewPaymentRecord extends AppCompatActivity {
                 findViewById(R.id.dateSelectButton)
         );
 
+        saveNewPaymentButton.setOnClickListener(v -> onSaveNewPaymentButtonClicked());
+    }
+
+    private void onSaveNewPaymentButtonClicked(){
+        Log.i("ADD_NEW_PAYMENT_RECORD_ACTIVITY_LOGS", "onSaveNewPaymentButtonClicked");
+        try{
+            this.processMoneyPaid();
+            dateTime.processTimeValue(timeDateDataSet);
+            dateTime.processDateValue(timeDateDataSet);
+
+            this.saveActivity();
+        } catch (Exception e) {
+            Log.i("ADD_NEW_PAYMENT_RECORD_ACTIVITY_LOGS", "onSaveNewPaymentButtonClicked exception: " + e);
+        }
+
+    }
+
+    private void processMoneyPaid() throws Exception{
+        Log.i("ADD_NEW_PAYMENT_RECORD_ACTIVITY_LOGS", "processMoneyPaid");
+        String moneyPaid = moneyPaidEditText.getText().toString().trim();
+        Log.i("ADD_NEW_PAYMENT_RECORD_ACTIVITY_LOGS", "moneyPaid value: " + moneyPaid);
+
+        if (TextUtils.isEmpty(moneyPaid)) {
+            moneyPaidEditText.setError("Field cannot be empty");
+            moneyPaidEditText.requestFocus();
+            throw new Exception("empty value");
+            // return;
+        }
+
+        // parse String to double
+        double parsedValue;
+        try {
+            String normalizedInput = moneyPaid.replace(",", ".");
+            parsedValue = Double.parseDouble(normalizedInput);
+        } catch (NumberFormatException e) {
+            moneyPaidEditText.setError("Cannot parse value to double");
+            throw new Exception("parse to double failed");
+        }
+
+        // set data to variable
+        this.moneyPaid = parsedValue;
+    }
+
+    private void saveActivity(){
+        Log.i("ADD_NEW_PAYMENT_RECORD_ACTIVITY_LOGS", "saveActivity");
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("MONEY_PAID", moneyPaid);
+        returnIntent.putExtra("TIME_DATA_DATA_SET", timeDateDataSet); // class requires serializable
+
+        setResult(RESULT_OK, returnIntent);
+        finish();
     }
 
     private void closeActivity(){
--- app/src/main/java/com/androsiukcezary/fuelcalculator/AddNewRefuelingRecord.java
+++ app/src/main/java/com/androsiukcezary/fuelcalculator/AddNewRefuelingRecord.java
@@ -1,8 +1,11 @@
 package com.androsiukcezary.fuelcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -14,12 +17,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddNewRefuelingRecord extends AppCompatActivity {
 
-    EditText m_currentFuelEditText;
-    Button m_saveNewRefuelingButton;
-    DateTimeStruct m_dateTime;
+    EditText refueledQuantityEditText;
+    EditText fuelPriceEditText;
+    CheckBox otherCarUserPaysCheckBox;
+    Button saveNewRefuelingButton;
+    DateTimeStruct dateTime;
+
+    double refueledQuantity;
+    double fuelPrice;
+    TimeDateDataSet timeDateDataSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("ADD_NEW_REFUELING_RECORD_ACTIVITY_LOGS", "onCreate");
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_new_refueling_record);
@@ -32,11 +42,16 @@ public class AddNewRefuelingRecord extends AppCompatActivity {
         ImageButton closeAddingRefuelingButton = (ImageButton) findViewById(R.id.closeAddingRefuelingButton);
         closeAddingRefuelingButton.setOnClickListener(v -> this.closeActivity());
 
-        m_currentFuelEditText = (EditText) findViewById(R.id.currentFuelEditText);
-        m_saveNewRefuelingButton = (Button) findViewById(R.id.saveNewRefuelingButton);
+        timeDateDataSet = new TimeDateDataSet();
+
+        ///  store Views in variables
+        refueledQuantityEditText = (EditText) findViewById(R.id.refueledQuantityEditText);
+        fuelPriceEditText = (EditText) findViewById(R.id.fuelPriceEditText);
+        otherCarUserPaysCheckBox = (CheckBox) findViewById(R.id.otherCarUserPaysCheckBox);
+        saveNewRefuelingButton = (Button) findViewById(R.id.saveNewRefuelingButton);
 
         ///  create date time structure and assign views to them
-        m_dateTime = new DateTimeStruct(
+        dateTime = new DateTimeStruct(
                 this,
                 findViewById(R.id.timeEditText),
                 findViewById(R.id.timeSelectButton),
@@ -44,6 +59,88 @@ public class AddNewRefuelingRecord extends AppCompatActivity {
                 findViewById(R.id.dateSelectButton)
         );
 
+        saveNewRefuelingButton.setOnClickListener(v -> this.onSaveNewRefuelingButtonClicked());
+    }
+
+    private void onSaveNewRefuelingButtonClicked(){
+        Log.i("ADD_NEW_REFUELING_RECORD_ACTIVITY_LOGS", "onSaveNewRefuelingButtonClicked");
+        try{
+            this.processRefueledQuantity();
+            this.processFuelPrice();
+            ///  No CheckBox process
+            dateTime.processTimeValue(timeDateDataSet);
+            dateTime.processDateValue(timeDateDataSet);
+
+            this.saveActivity();
+        } catch (Exception e) {
+            Log.i("ADD_NEW_REFUELING_RECORD_ACTIVITY_LOGS", "onSaveNewRefuelingButtonClicked exception: " + e);
+        }
+
+    }
+
+    private void processRefueledQuantity() throws Exception{
+        Log.i("ADD_NEW_REFUELING_RECORD_ACTIVITY_LOGS", "processRefueledQuantity");
+        String refueledQuantity = refueledQuantityEditText.getText().toString().trim();
+        Log.i("ADD_NEW_REFUELING_RECORD_ACTIVITY_LOGS", "refueledQuantity value: " + refueledQuantity);
+
+        if (TextUtils.isEmpty(refueledQuantity)) {
+            refueledQuantityEditText.setError("Field cannot be empty");
+            refueledQuantityEditText.requestFocus();
+            throw new Exception("empty value");
+            // return;
+        }
+
+        // parse String to double
+        double parsedValue;
+        try {
+            String normalizedInput = refueledQuantity.replace(",", ".");
+            parsedValue = Double.parseDouble(normalizedInput);
+        } catch (NumberFormatException e) {
+            refueledQuantityEditText.setError("Cannot parse value to double");
+            throw new Exception("parse to double failed");
+        }
+
+        // set data to variable
+        this.refueledQuantity = parsedValue;
+    }
+
+    private void processFuelPrice() throws Exception{
+        Log.i("ADD_NEW_REFUELING_RECORD_ACTIVITY_LOGS", "processFuelPrice");
+        String fuelPrice = fuelPriceEditText.getText().toString().trim();
+        Log.i("ADD_NEW_REFUELING_RECORD_ACTIVITY_LOGS", "fuelPrice value: " + fuelPrice);
+
+        if (TextUtils.isEmpty(fuelPrice)) {
+            fuelPriceEditText.setError("Field cannot be empty");
+            fuelPriceEditText.requestFocus();
+            throw new Exception("empty value");
+            // return;
+        }
+
+        // parse String to double
+        double parsedValue;
+        try {
+            String normalizedInput = fuelPrice.replace(",", ".");
+            parsedValue = Double.parseDouble(normalizedInput);
+        } catch (NumberFormatException e) {
+            fuelPriceEditText.setError("Cannot parse value to double");
+            throw new Exception("parse to double failed");
+        }
+
+        // set data to variable
+        this.fuelPrice = parsedValue;
+    }
+
+    private void saveActivity(){
+        Log.i("ADD_NEW_REFUELING_RECORD_ACTIVITY_LOGS", "saveActivity");
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("REFUELED_QUANTITY", refueledQuantity);
+        returnIntent.putExtra("FUEL_PRICE", fuelPrice);
+        returnIntent.putExtra("OTHER_CAR_USER_PAYS", otherCarUserPaysCheckBox.isChecked());
+        returnIntent.putExtra("TIME_DATA_DATA_SET", timeDateDataSet); // class requires serializable
+
+        setResult(RESULT_OK, returnIntent);
+        finish();
     }
 
     private void closeActivity(){
